28b71e213e88c019f4ca7d9faf292781177de02c
Bek <152039164+bekzzzz@users.noreply.github.com>
2026-01-02 10:59:11 +0700

    Delete script directory

--- script/img.scroll-bar.js
+++
@@ -1,8 +0,0 @@
-function scrolll() {
-  var left = document.querySelector(".scroll-images");
-  left.scrollBy(1100, 0);
-}
-function scrollr() {
-  var right = document.querySelector(".scroll-images");
-  right.scrollBy(-1100, 0);
-}
